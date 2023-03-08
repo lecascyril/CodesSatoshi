20a1571aac82aced80e6fbd56eb218d107731df6
Castagnet Cyril <45956179+lecascyril@users.noreply.github.com>
2023-03-08 23:43:02 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { EthProvider } from "./contexts/EthContext";
+import Web3stuff from "./components/Web3stuff/";
+
+
+function App() {
+  return (
+    <EthProvider>
+      <div id="App">
+        <div className="container">
+          <Web3stuff />
+        </div>
+      </div>
+    </EthProvider>
+  );
+}
+
+export default App;
